f28b7f1f6e99c0190f20966e1f9c4f0e6207b690
Lili <fjzll.nju@gmail.com>
2024-08-23 12:50:01 +0800

    assert floating point number

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
+import math
+
 def add(a, b):
     return a + b
 
@@ -5,3 +7,6 @@ def add(a, b):
 def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
+    assert math.isclose(add(0.1, 0.2), 0.3)
+
+
